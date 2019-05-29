7b315e3679b4f0fe8dbdb6ef75a39b7ad80af82b
胡Eis <583860807@qq.com>
2019-05-29 16:00:40 +0800

    Initial commit

---
+++ CRF_disease.py
@@ -0,0 +1,45 @@
+from nltk.corpus import PlaintextCorpusReader
+import nltk
+import textblob
+import re
+
+import pandas as pd
+
+cr="C:\\Users\\dell\\PycharmProjects\\LBD_test\\corpus"
+pmids=[]
+text=[]
+with open(cr+"\\NCBI_corpus_training.txt","r") as fp:
+    strings=fp.readlines()
+for i in strings:
+    string_split=i.split('\t')
+    pmids.append(string_split[0])
+    string_split.pop(0)
+    text.append(' '.join(string_split))
+
+mydf=pd.DataFrame()
+mydf['pmid']=pmids
+mydf['text']=text
+sents_no=[]
+pmids=[]
+i=0
+for index,row in mydf.iterrows():
+    stence=row['text']
+    sents=stence.split('.')
+    newsents=[]
+    for sent in sents:
+        newsents.append(sent+'.')
+        i=i+1
+        sents_no.append(i)
+        pmids.append(row['pmid'])
+
+    # result= re.findall(r"<category=\".`+?\">(.+?)</category>",stence, re.S)
+    # print(result)
+print(sents_no)
+print(pmids)
+
+# wodslist = PlaintextCorpusReader(cr, '.*')
+#
+# for i in wodslist.sents('NCBI_corpus_training.txt'):
+#     text=nltk.word_tokenize(' '.join(i))
+#
+#     print(nltk.pos_tag(text, tagset='universal'))
